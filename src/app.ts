0aef26f6a78c153545b500beb281df870923836b
Inayat Cassambai <inu@Inayats-MacBook-Pro.local>
2021-04-02 11:55:05 +0100

    cleanup

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,7 @@
-import express, { Application, Request, Response, NextFunction } from 'express';
-import helmet from 'helmet';
+import express, { Application, Request, Response, NextFunction } from "express";
+import helmet from "helmet";
 
-import { routes } from './routes';
-import { CreateError } from '@util/CreateError';
+import { CreateError } from "@util/CreateError";
 
 const app: Application = express();
 
@@ -13,30 +12,30 @@ const app: Application = express();
  */
 app.use(
     helmet({
-        contentSecurityPolicy: process.env.NODE_ENV === 'production' ? undefined : false,
-    }),
+        contentSecurityPolicy:
+            process.env.NODE_ENV === "production" ? undefined : false,
+    })
 );
 
 // Parses incoming JSON requests
 app.use(express.json());
 
-// Connect all the routes in our application
-app.use('/', routes);
-
 // Catch our errors
-app.use((err: CreateError, _req: Request, res: Response, _nxt: NextFunction) => {
-    const { stack, message } = err;
+app.use(
+    (err: CreateError, _req: Request, res: Response, _nxt: NextFunction) => {
+        const { stack, message } = err;
 
-    if (err instanceof CreateError) {
-        res.status(err.code).json(err.message);
-        return;
+        if (err instanceof CreateError) {
+            res.status(err.code).json(err.message);
+            return;
+        }
+
+        res.status(500).json({
+            default: "Internal Server Error, something went wrong",
+            message,
+            stack,
+        });
     }
-
-    res.status(500).json({
-        default: 'Internal Server Error, something went wrong',
-        message,
-        stack,
-    });
-});
+);
 
 export { app };
